1ddb7837d78aff9e18df1d4c45b8aa0b10a1ca77
axlevest <axlevest@gmail.com>
2017-08-17 11:51:33 +1000

    Add files via upload
    Upload of existing files

---
+++ js/tab.js
@@ -0,0 +1,18 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).style.boxShadow = "0 0 5px rgba(0,0,0,0.5)";
+    evt.currentTarget.className += " active";
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
